48fe21642ef343b667754c5b14175461ffb9f13e
Hugo <mail@hugodaniel.pt>
2019-07-19 19:15:42 +0100

    Nunjucks

--- packages/meander/fuse.ts
+++ packages/meander/fuse.ts
@@ -1,18 +1,28 @@
-import { fusebox } from "fuse-box";
+// import { fusebox } from "fuse-box";
+import nunjucks from "nunjucks-watch";
 
+nunjucks.watch({
+	src: './index.njk',
+	dest: 'build/index.html'
+})
+
+/*
 const fuse = fusebox({
-  target: "browser",
-  entry: "src/index.ts",
-  webIndex: {
-    template: "src/index.html"
-  },
-  cache: {
-    root: ".cache",
-    enabled: true
-  },
-  watch: true,
-  hmr: true,
-  devServer: true,
-  logging: { level: "succinct" }
+	target: "browser",
+	entry: "src/index.ts",
+	webIndex: {
+		template: "build/index.html"
+	},
+	cache: {
+		root: ".cache",
+		enabled: false
+	},
+	watch: true,
+	hmr: true,
+	devServer: true,
+	logging: { level: "succinct" }
 });
 fuse.runDev();
+fs.writeFile('build/index.html', index, () => {
+});
+*/
--- packages/meander/src/index.ts
+++ packages/meander/src/index.ts
@@ -1,25 +1,18 @@
-import { classify } from "./utils";
-
-// TODO: a template theme
-// recursively searches for the parent element of type 'name'
-function parentName(elem: Element, name: string): Element {
-  if (elem.nodeName === name || elem.nodeName === "BODY") return elem;
-  return parentName(elem.parentElement, name);
+const dom = document;
+function meander() {
+	console.log('LOADED')
+	// Remove the loading for now
+	const loading = dom.getElementById('loading')
+	loading.classList.add('dn')
+	loading.classList.remove('flex')
 }
+dom.addEventListener("DOMContentLoaded", meander);
 
-function meander(theme: MeanderTheme) {
-  const dom = document;
-  dom.addEventListener("DOMContentLoaded", (e: Event) => {
-    const sectionTitles: string[] = [];
-    const sectionElements: Element[] = [];
-    dom.querySelectorAll("#meander section h1").forEach(elem => {
-      const parent = parentName(elem, "SECTION");
-      // insert the parent and its title if not already there
-      if (sectionElements.indexOf(parent) === -1) {
-        sectionElements.push(parent);
-        sectionTitles.push(elem.textContent);
-      }
-    });
-    console.log(sectionTitles, sectionElements);
-  });
-}
+/*
+Actions:
+exit
+startLoading
+stopLoading
+goto() // login | about | projects ...
+
+*/
