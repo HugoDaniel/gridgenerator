d8d7c38fbcf7000b44766168e312d646db0d16c9
Hugo <mail@hugodaniel.pt>
2019-07-23 20:10:22 +0100

    Rendering multiple pages

--- packages/meander/fuse.ts
+++ packages/meander/fuse.ts
@@ -1,10 +1,49 @@
 // import { fusebox } from "fuse-box";
-import nunjucks from "nunjucks-watch";
+import fs from 'fs-extra';
+import nunjucks from 'nunjucks';
+import chokidar from 'chokidar';
+import klaw from 'klaw';
 
-nunjucks.watch({
-	src: './index.njk',
-	dest: 'build/index.html'
-})
+const env = nunjucks.configure('.', { noCache: true });
+env.addFilter('take', function (str, count) {
+	return str.slice(0, count || 5);
+});
+
+const renderPage = (page: string) => {
+	env.render(`./pages/${page}`, {}, function (err: string, data: string) {
+		if (err) {
+			console.log(err);
+		} else {
+			const dest = `build/${page.split('.')[0]}.html`;
+			fs.writeFile(dest, data, (err) => {
+				if (err) throw err;
+				else
+					console.log('Wrote ', dest)
+			});
+		}
+	});
+}
+
+let isWatching = false;
+const watch = () => {
+	if (isWatching) return;
+	isWatching = true;
+	chokidar.watch('.', { ignored: 'build' }).on('change', (event, path) => {
+		renderPages()
+	});
+}
+
+fs.copy('assets', 'build/assets');
+const renderPages = () => {
+	klaw('pages').on('data', item => {
+		if (!item.stats.isDirectory()) {
+			renderPage(item.path.split('/').pop());
+		}
+	}).on('end', watch)
+}
+
+renderPages()
+
 
 /*
 const fuse = fusebox({
